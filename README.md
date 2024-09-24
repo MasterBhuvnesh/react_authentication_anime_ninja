4bbc60a5962f8d48c69334a58ccfd55dc40b0527
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-09-25 00:20:59 +0530

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,67 @@
-# Getting Started with Create React App
+## Create your project
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+**Start by creating a new React project with Create React App v5.0+ if you don't have one already set up.**
 
-## Available Scripts
+```bash
+npm init react-app anime-ninja
+cd anime-ninja
+```
 
-In the project directory, you can run:
+## Install Tailwind CSS
 
-### `npm start`
+**Install `tailwindcss` via npm, and then run the init command to generate your `tailwind.config.js` file.**
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+```bash
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+```
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Configure your template paths
 
-### `npm test`
+**Add the paths to all of your template files in your `tailwind.config.js` file. & Also add Poppins font link in `.public/index.html` file.**
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+```javascript
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+};
+```
 
-### `npm run build`
+````html
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+  rel="stylesheet"
+/>
+``` ## Add the Tailwind directives to your CSS **Add the `@tailwind` directives
+for each of Tailwind’s layers to your `./src/index.css` file.** ```css @tailwind
+base; @tailwind components; @tailwind utilities;
+````
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Start your build process
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Run your build process with `npm run start`.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+```bash
+npm run start
+```
 
-### `npm run eject`
+## Then for `headlessui` & `heroicons`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+```bash
+npm install tailwindcss@latest
+npm install @headlessui/react @heroicons/react
+```
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Add auth0
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+```bash
+npm install @auth0/auth0-react
+```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import Profile from "./components/Profile";
+import icon from "./images/icon.png";
+
+const navigation = [
+  { name: "Portfolio", href: "https://masterbhuvnesh.github.io/portfolio/" },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/bhuvneshverma/" },
+  { name: "Github", href: "https://github.com/MasterBhuvnesh" },
+  {
+    name: "Code",
+    href: "https://github.com/MasterBhuvnesh/react_authentication_anime_ninja",
+  },
+];
 
 function App() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { isAuthenticated } = useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="bg-white font-poppins bg-gradient-to-r from-blue-500">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          aria-label="Global"
+          className="flex items-center justify-between p-6 lg:px-8"
         >
-          Learn React
-        </a>
+          <div className="flex lg:flex-1">
+            <span className="sr-only">Auth0 - Template</span>
+            <img
+              alt="Auth0 - Authentication"
+              src={icon}
+              className="h-8 w-auto"
+            />
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="h-6 w-6"
+              />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end ">
+            <div className="text-sm font-semibold leading-6 text-gray-900 font-poppins ">
+              <LoginButton />
+              <LogoutButton />
+            </div>
+          </div>
+        </nav>
+        <Dialog
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+          className="lg:hidden"
+        >
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <span className="sr-only">Auth0 - Template</span>
+              <img
+                alt="Auth0 - Authentication"
+                src={icon}
+                className="h-8 w-auto"
+              />
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon
+                  aria-hidden="true"
+                  className="h-6 w-6"
+                />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-normal leading-7 text-gray-900 hover:bg-gray-50 font-poppins"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <div className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-normal leading-7 text-gray-900 hover:bg-gray-50 font-poppins">
+                    <LoginButton />
+                    <LogoutButton />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
       </header>
+      {isAuthenticated ? (
+        <Profile />
+      ) : (
+        <div class="overflow-hidden bg-white py-24 sm:py-32">
+          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div class="lg:pr-8 lg:pt-4">
+                <div class="lg:max-w-lg">
+                  <h2 class="text-base font-semibold leading-7 text-indigo-600">
+                    Bhuvnesh Verma
+                  </h2>
+                  <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                    The Magic of Anime
+                  </p>
+                  <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                    <div class="relative pl-9">
+                      <dt class="inline font-semibold text-gray-900  text-lg pr-4">
+                        Anime -
+                      </dt>
+                      <dd class="inline">
+                        Japanese animation brings worlds to life 🎨✨ with
+                        stunning art, unforgettable characters, and a mix of
+                        genres that keeps you on the edge of your seat! 🌟 Dive
+                        into classics like Naruto, Attack on Titan, and My Hero
+                        Academia!
+                      </dd>
+                    </div>
+                    <div class="relative pl-9">
+                      <dt class="inline font-semibold text-gray-900 text-lg pr-4">
+                        Manga -
+                      </dt>
+                      <dd class="inline">
+                        Japanese manga 📚✨ are graphic novels that capture
+                        hearts with their black-and-white artistry and
+                        right-to-left storytelling! 🌟 Dive into epic series
+                        like One Piece and Demon Slayer that inspire incredible
+                        anime adaptations!
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+              <img
+                src="https://i.pinimg.com/originals/dd/9d/3d/dd9d3db28020faecfec325982ed24227.png"
+                alt="Product screenshot"
+                class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                width="2432"
+                height="1442"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
